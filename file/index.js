b5a320440cb2fb68317a83fc68047deaf15cac42
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-03-11 23:36:13 +0530

    App Opener

--- file/index.js
+++ file/index.js
@@ -4,3 +4,12 @@ setTimeout(function(){
   $(".web").css("display", "block");
   $(".preloader").css("display", "none");
 }, 3000);
+
+Context contex = this;
+try {
+  //just use package name of your app which you want to open
+  Intent i = contex.getPackageManager().getLaunchIntentForPackage("com.twidroid.SendTweet");
+  contex.startActivity(i);
+} catch (NameNotFoundException e) {
+  // TODO Auto-generated catch block
+}
